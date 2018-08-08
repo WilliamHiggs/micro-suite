6bf28f006157cbea97ad54376651b873f7745b65
William Higgs <32944349+WilliamHiggs@users.noreply.github.com>
2018-08-08 15:39:42 +0100

    toInclude and toNotInclude
    two new features on the expect api and better code format

--- test.js
+++ test.js
@@ -1,159 +1,182 @@
+
 const underline = '\x1b[4m',
-	    reset = '\x1b[0m',
-	    green = '\x1b[32m',
-	    red = '\x1b[31m';
+  reset = '\x1b[0m',
+  green = '\x1b[32m',
+  red = '\x1b[31m';
 
 function objectEquals(x, y) {
-	'use strict';
+  'use strict';
 
-	if (x === null || x === undefined || y === null || y === undefined) {
-		return x === y;
-	}
+  if (x === null || x === undefined || y === null || y === undefined) {
+    return x === y;
+  }
 
-	if (x.constructor !== y.constructor) {
-		return false;
-	}
+  if (x.constructor !== y.constructor) {
+    return false;
+  }
 
-	if (x instanceof Function) {
-		return x === y;
-	}
+  if (x instanceof Function) {
+    return x === y;
+  }
 
-	if (x instanceof RegExp) {
-		return x === y;
-	}
+  if (x instanceof RegExp) {
+    return x === y;
+  }
 
-	if (x === y || x.valueOf() === y.valueOf()) {
-		return true;
-	}
+  if (x === y || x.valueOf() === y.valueOf()) {
+    return true;
+  }
 
-	if (Array.isArray(x) && x.length !== y.length) {
-		return false;
-	}
+  if (Array.isArray(x) && x.length !== y.length) {
+    return false;
+  }
 
-	if (x instanceof Date) {
-		return false;
-	}
+  if (x instanceof Date) {
+    return false;
+  }
 
-	if (!(x instanceof Object)) {
-		return false;
-	}
+  if (!(x instanceof Object)) {
+    return false;
+  }
 
-	if (!(y instanceof Object)) {
-		return false;
-	}
+  if (!(y instanceof Object)) {
+    return false;
+  }
 
-	var p = Object.keys(x);
-	return (
-		Object.keys(y).every(function(i) {
-			return p.indexOf(i) !== -1;
-		}) &&
-		p.every(function(i) {
-			return objectEquals(x[i], y[i]);
-		})
-	);
+  var p = Object.keys(x);
+  return (
+    Object.keys(y).every(function (i) {
+      return p.indexOf(i) !== -1;
+    }) &&
+    p.every(function (i) {
+      return objectEquals(x[i], y[i]);
+    })
+  );
 }
 
 function describe(description, it) {
-	console.log('\n', underline, description, reset);
-	it();
+  console.log('\n', underline, description, reset);
+  it();
 }
 
 function it(should, expect) {
-	console.log(should);
-	expect();
+  console.log(should);
+  expect();
 }
 
 function expect(actual) {
-	console.time('time');
-	console.log('returned: ' + actual);
-	console.timeEnd('time');
-	return {
-		toBe: function(expected) {
-			if (typeof actual === typeof expected) {
-				if (objectEquals(actual, expected)) {
-					console.log(green, "✔ TEST PASSED." + "\n", reset);
-				} else {
-					console.log(
-						red,
-						`✗ TEST FAILED. Expected ${expected} but got ${actual}` + '\n',
-						reset
-					);
-				}
-			} else {
-				console.log(
-					red,
-					`✗ TEST FAILED. Wrong type. Expected ${typeof expected} but got ${typeof actual}` +
-						'\n',
-					reset
-				);
-			}
-		},
-		notToBe: function(expected) {
-			if (!objectEquals(actual, expected)) {
-				console.log(green, "✔ TEST PASSED." + "\n", reset);
-			} else {
-				console.log(
-					red,
-					`✗ TEST FAILED. ${expected} equals ${actual}` + '\n',
-					reset
-				);
-			}
-		},
-		toBeMoreThan: function(expected) {
-			if (typeof expected === 'number' && typeof actual === 'number') {
-				if (actual > expected) {
-					console.log(green, "✔ TEST PASSED." + "\n", reset);
-				} else if (actual === expected) {
-					console.log(
-						red,
-						`✗ TEST FAILED. ${actual} is equal to ${expected}  ` + '\n',
-						reset
-					);
-				} else {
-					console.log(
-						red,
-						`✗ TEST FAILED. ${actual} is less than ${expected}  ` + '\n',
-						reset
-					);
-				}
-			} else {
-				console.log(
-					red,
-					`✗ TEST FAILED. Requires a number for both conditions` + '\n',
-					reset
-				);
-			}
-		},
-		toBeLessThan: function(expected) {
-			if (typeof expected === 'number' && typeof actual === 'number') {
-				if (actual < expected) {
-					console.log(green, "✔ TEST PASSED." + "\n", reset);
-				} else if (actual === expected) {
-					console.log(
-						red,
-						`✗ TEST FAILED. ${actual} is equal to ${expected}  ` + '\n',
-						reset
-					);
-				} else {
-					console.log(
-						red,
-						`✗ TEST FAILED. ${actual} is more than ${expected}  ` + '\n',
-						reset
-					);
-				}
-			} else {
-				console.log(
-					red,
-					`✗ TEST FAILED. Requires a number for both conditions` + '\n',
-					reset
-				);
-			}
-		}
-	};
+  console.time('time');
+  console.log('returned: ' + actual);
+  console.timeEnd('time');
+  return {
+    toBe: function (expected) {
+      if (typeof actual === typeof expected) {
+        if (objectEquals(actual, expected)) {
+          console.log(green, "✔ TEST PASSED." + "\n", reset);
+        } else {
+          console.log(
+            red,
+            `✗ TEST FAILED. Expected ${expected} but got ${actual}` + '\n',
+            reset
+          );
+        }
+      } else {
+        console.log(
+          red,
+          `✗ TEST FAILED. Wrong type. Expected ${typeof expected} but got ${typeof actual}` +
+          '\n',
+          reset
+        );
+      }
+    },
+    notToBe: function (expected) {
+      if (!objectEquals(actual, expected)) {
+        console.log(green, "✔ TEST PASSED." + "\n", reset);
+      } else {
+        console.log(
+          red,
+          `✗ TEST FAILED. ${expected} equals ${actual}` + '\n',
+          reset
+        );
+      }
+    },
+    toBeMoreThan: function (expected) {
+      if (typeof expected === 'number' && typeof actual === 'number') {
+        if (actual > expected) {
+          console.log(green, "✔ TEST PASSED." + "\n", reset);
+        } else if (actual === expected) {
+          console.log(
+            red,
+            `✗ TEST FAILED. ${actual} is equal to ${expected}  ` + '\n',
+            reset
+          );
+        } else {
+          console.log(
+            red,
+            `✗ TEST FAILED. ${actual} is less than ${expected}  ` + '\n',
+            reset
+          );
+        }
+      } else {
+        console.log(
+          red,
+          `✗ TEST FAILED. Requires a number for both conditions` + '\n',
+          reset
+        );
+      }
+    },
+    toBeLessThan: function (expected) {
+      if (typeof expected === 'number' && typeof actual === 'number') {
+        if (actual < expected) {
+          console.log(green, "✔ TEST PASSED." + "\n", reset);
+        } else if (actual === expected) {
+          console.log(
+            red,
+            `✗ TEST FAILED. ${actual} is equal to ${expected}  ` + '\n',
+            reset
+          );
+        } else {
+          console.log(
+            red,
+            `✗ TEST FAILED. ${actual} is more than ${expected}  ` + '\n',
+            reset
+          );
+        }
+      } else {
+        console.log(
+          red,
+          `✗ TEST FAILED. Requires a number for both conditions` + '\n',
+          reset
+        );
+      }
+    },
+    toInclude: function (expected) {
+      if (actual.includes(expected)) {
+        console.log(green, "✔ TEST PASSED." + "\n", reset);
+      } else {
+        console.log(
+          red,
+          `✗ TEST FAILED. Result did not include ${expected}` + '\n',
+          reset
+        );
+      }
+    },
+    toNotInclude: function (expected) {
+      if (!actual.includes(expected)) {
+        console.log(green, "✔ TEST PASSED." + "\n", reset);
+      } else {
+        console.log(
+          red,
+          `✗ TEST FAILED. Result did include ${expected}` + '\n',
+          reset
+        );
+      }
+    }
+  };
 }
 
 module.exports = {
-	describe: describe,
-	it: it,
-	expect: expect
+  describe: describe,
+  it: it,
+  expect: expect
 };
